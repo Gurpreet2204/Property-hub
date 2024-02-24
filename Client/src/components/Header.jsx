8ea97e42405a5529b16929f227e1b4d3a0d8c2a9
Gurpreet Singh <gurs2806@gmail.com>
2024-02-24 22:42:32 +0530

    created backend nodejs

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,41 +1,41 @@
 import React from 'react'
-import {FaSearch} from 'react-icons/fa'
+import { FaSearch } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 export default function Header() {
-  return (
-    <header className=' bg-red-200 shadow-md' >
-        <div className=' flex justify-between items-center max-w-6xl mx-auto p-3'>
-            <Link to='/'>
-            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-red-500'> Property</span>
-            <span className='text-black'> hub</span>
-        </h1>
-            </Link>
-            
-        <form className='bg-red-100 p-3 rounded-lg flex items-center'>
-            <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-            <FaSearch className='text-red-500' />
-        </form>
-        
-        <ul className='flex gap-4'>
-            <Link to='/'>
-            <li className='hidden sm:inline text-black hover:underline'>
-                Home
-                </li>
+    return (
+        <header className=' bg-red-200 shadow-md' >
+            <div className=' flex justify-between items-center max-w-6xl mx-auto p-3'>
+                <Link to='/'>
+                    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                        <span className='text-red-500'> Property</span>
+                        <span className='text-black'> hub</span>
+                    </h1>
                 </Link>
-                <Link to='about'>
-            <li className='hidden sm:inline text-black hover:underline'>
-                Abuot
-                </li>
-                </Link>
-                <Link to='sign-in'>
-            <li className=' text-black hover:underline'>
-                Sign in
-                </li>
-                </Link>
-        </ul>
-        </div>
-        
-    </header>
-  )
+
+                <form className='bg-red-100 p-3 rounded-lg flex items-center'>
+                    <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
+                    <FaSearch className='text-red-500' />
+                </form>
+
+                <ul className='flex gap-4'>
+                    <Link to='/'>
+                        <li className='hidden sm:inline text-black hover:underline'>
+                            Home
+                        </li>
+                    </Link>
+                    <Link to='about'>
+                        <li className='hidden sm:inline text-black hover:underline'>
+                            Abuot
+                        </li>
+                    </Link>
+                    <Link to='sign-in'>
+                        <li className=' text-black hover:underline'>
+                            Sign in
+                        </li>
+                    </Link>
+                </ul>
+            </div>
+
+        </header>
+    )
 }
