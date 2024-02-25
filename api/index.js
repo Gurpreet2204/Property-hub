1f59fe374885abb82adb3895126652f64708856a
Gurpreet Singh <gurs2806@gmail.com>
2024-02-25 15:10:25 +0530

    .

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
 const app = express();
-
+mongoose.connect("")
 app.listen(3000, () => {
   console.log("Server is running in port 3000!");
 });
