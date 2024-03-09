9ecf9d7b36f719fb795e1b17991f0159d192a41b
Gurpreet Singh <gurs2806@gmail.com>
2024-03-09 18:53:55 +0530

    sign-up

--- Client/src/pages/Singup.jsx
+++ Client/src/pages/Singup.jsx
@@ -1,41 +1,83 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 export default function Singup() {
-  return (
-    <div className='p-3 max-w-lg mx-auto '>
-      <h1 className='text-3xl text-center font-semibold
-      my-7'>
-        Signup
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true)
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+    setLoading(false)
+    if (data.success===false){
+      setError(data.message)
+      return;
+    }
+    setLoading(data); 
+    console.log(data);
+  };
+  console.log(formData);
+  return (
+    <div className="p-3 max-w-lg mx-auto ">
+      <h1
+        className="text-3xl text-center font-semibold
+      my-7"
+      >
+        Signup
       </h1>
 
-      <form className='flex flex-col gap-4 '>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
 
-        <input type="text" placeholder='username' className='border p-3 rounded-lg'
-        
-        id='usename'
-        />
-        <input type="email" placeholder='email' className='border p-3 rounded-lg'
-        
-        id='email'
-        />
-        <input type="password" placeholder='password' className='border p-3 rounded-lg'
-        
-        id='password'
-        />
-
-        <button className='bg-slate-700 text-white p-3 
-        rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+        <button
+          className=" bg-red-500 text-white p-3 
+        rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
           sign up
         </button>
       </form>
-      <div className='flex gap-2 mt-5'>
+      <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
-        <Link to='/sign-in'>
-         <span className='text-blue-700'>Signin</span>
+        <Link to="/sign-in">
+          <span className="text-blue-700">Signin</span>
         </Link>
       </div>
     </div>
-  )
+  );
 }
--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use("/api/auth", userAuth);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "INternal server error";
+  const message = err.message || "Internal server error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
